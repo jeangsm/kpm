338bccaf9d617881e25d2e20a597830df4db6398
jeangsm <jeangabrielsm2009@hotmail.com>
2017-09-13 10:57:16 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,13 +3,13 @@
 
 function scripts_do_template() {
     // Bootstrap core JavaScript
-    wp_register_script('bootstrap', get_template_directory_uri().'/js/bootstrap.min.js', array('jquery'));
+    wp_register_script('bootstrapjs', get_template_directory_uri().'/js/bootstrap.min.js', array('jquery'));
     // Bootstrap core CSS
     wp_register_style('bootstrapcss', get_template_directory_uri().'/css/bootstrap.min.css');
     wp_register_style('bootstrapgridcss', get_template_directory_uri().'/css/bootstrap-grid.min.css');
 
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap');
+    wp_enqueue_script('bootstrapjs');
     wp_enqueue_script('bootstrapcss');
     wp_enqueue_script('bootstrapgridcss');
 }
