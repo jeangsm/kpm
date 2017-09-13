3ea7c09d312b1b266a1fdd7e086493313c837ff7
jeangsm <jeangabrielsm2009@hotmail.com>
2017-09-13 11:01:20 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="<?php bloginfo('language') ?>">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title></title>
+	<?php wp_head() ?>
 </head>
 <body>
 	
