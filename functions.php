0678e699f7b26bfce8b224eeb45a3d844f59acae
jeangsm <jeangabrielsm2009@hotmail.com>
2017-09-13 10:35:10 -0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+@package kpm
+
+function scripts_do_template() {
+    // Bootstrap core JavaScript
+    wp_register_script('bootstrap', get_template_directory_uri().'/js/bootstrap.min.js', array('jquery'));
+    // Bootstrap core CSS
+    wp_register_style('bootstrapcss', get_template_directory_uri().'/css/bootstrap.min.css');
+    wp_register_style('bootstrapgridcss', get_template_directory_uri().'/css/bootstrap-grid.min.css');
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap');
+    wp_enqueue_script('bootstrapcss');
+    wp_enqueue_script('bootstrapgridcss');
+}
+
+add_action('wp_enqueue_scripts', 'scripts_do_template');
+
+
+?>
