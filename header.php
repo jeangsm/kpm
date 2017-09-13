c1816eaa43fc50363f0d420a293dca07417e20d0
jeangsm <jeangabrielsm2009@hotmail.com>
2017-09-13 12:19:07 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php
+	@package kpm
+?>
 <!DOCTYPE html>
 <html lang="<?php bloginfo('language') ?>">
 <head>
@@ -6,4 +9,29 @@
 	<?php wp_head() ?>
 </head>
 <body>
+	<nav class="navbar navbar-expand-md navbar-light fixed-top bg-white">
+		<a class="navbar-brand" href="#">KPM Tax</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+      		</button>
+      		<div class="collapse navbar-collapse" id="navbarCollapse">
+        		<ul class="navbar-nav mr-auto">
+          			<li class="nav-item active">
+            				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          			</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Disabled</a>
+				</li>
+        		</ul>
+        		<form class="form-inline mt-2 mt-md-0">
+          			<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        		</form>
+      		</div>
+    	</nav>
+	<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+
 	
